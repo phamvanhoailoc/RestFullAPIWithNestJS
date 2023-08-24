e9eaf191677f3923d180c8ab77c0d2da72589a61
phamvanhoailoc <phamvanhoailoc0507@gmail.com>
2023-08-24 20:30:58 +0700

    Thêm quan hệ one-to-many vào Database Schema và migrations lại những thay đổi

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,15 +10,28 @@ export class AuthService{
     constructor(private prismaService: PrismaService){}
     async register(authDTO: AuthDTO){
         const hashedPassword = await argon.hash(authDTO.password)
-        const user = await this.prismaService.user.create({
-            data:{
-                email: authDTO.email,
-                hashedPassword: hashedPassword,
-                firstName: '',
-                lastName: ''
-            }
-        })
+        try{
+            const user = await this.prismaService.user.create({
+                data:{
+                    email: authDTO.email,
+                    hashedPassword: hashedPassword,
+                    firstName: '',
+                    lastName: ''
+                },
+                //only select id, email, createdAt
+                select:{
+                    id: true,
+                    email: true,
+                    createdAt : true
+                }
+            })
         return user
+
+        }catch(err){
+            return {
+                err
+            }
+        }
     }
     login(){
         return {
